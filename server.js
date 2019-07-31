eefd79d86e20e9263b70dd322cbc0cbf4c6c4016
Nithin <chamala.nithinreddy@dish.com>
2019-07-31 17:51:29 +0530

    commited for test

--- server.js
+++ server.js
@@ -5,14 +5,7 @@ var server = http.createServer(function(request, response) {
     var path = url.parse(request.url).pathname;
     switch (path) {
         case '/':
-            response.writeHead(200, {
-                'Content-Type': 'text/plain'
-            });
-            response.write("This is Test Message.");
-            response.end();
-            break;
-        case '/index.html':
-            fs.readFile(__dirname + path, function(error, data) {
+            fs.readFile(__dirname + '/index.html', function(error, data) {
                 if (error) {
                     response.writeHead(404);
                     response.write(error);
